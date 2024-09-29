1d46f937636ee8df8eae5f482606d9c1c6c171a0
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2024-09-29 19:40:04 -0300

    Adicionar Javadoc para a classe SimpleMath

--- src/main/java/br/com/nt/automated/tests/with/java/math/SimpleMath.java
+++ src/main/java/br/com/nt/automated/tests/with/java/math/SimpleMath.java
@@ -1,5 +1,29 @@
 package br.com.nt.automated.tests.with.java.math;
 
+/**
+ * Classe SimpleMath
+ * <p>
+ * Esta classe fornece métodos para realizar operações matemáticas básicas, como soma, subtração,
+ * multiplicação, divisão, cálculo da média e raiz quadrada. Ela é projetada para trabalhar com
+ * números do tipo {@code Double}, garantindo precisão para operações que envolvem casas decimais.
+ * <p>
+ * Todos os métodos garantem que os números fornecidos sejam não-negativos, e a divisão por zero
+ * resulta em uma exceção {@code IllegalArgumentException}.
+ *
+ * <p>Operações disponíveis:
+ * <ul>
+ *     <li>Soma</li>
+ *     <li>Subtração</li>
+ *     <li>Multiplicação</li>
+ *     <li>Divisão</li>
+ *     <li>Cálculo da média</li>
+ *     <li>Raiz quadrada</li>
+ * </ul>
+ *
+ * @author ediwaldo neto
+ * @version 1.0
+ * @since 29/09/2024
+ */
 public class SimpleMath {
 
 
