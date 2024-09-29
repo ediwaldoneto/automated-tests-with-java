162c915701c363e3be4404a86087105d5583559a
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2024-09-28 22:41:09 -0300

    :tada: Commit inicial

---
+++ src/test/java/br/com/nt/automated/tests/with/java/AutomatedTestsWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nt.automated.tests.with.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestsWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
